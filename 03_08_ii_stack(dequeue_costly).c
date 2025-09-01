c242f1b6038c638e85b65651dacac0f2ec014cc8
anishraj093 <131684006+anishraj093@users.noreply.github.com>
2025-09-01 23:37:32 +0530

    03_08

---
+++ 03_08_ii_stack(dequeue_costly).c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#define SIZE 10
+
+int queue1[SIZE], queue2[SIZE];
+int front1 = -1, rear1 = -1;
+int front2 = -1, rear2 = -1;
+
+// Queue operations
+int isEmpty1() { return front1 == -1; }
+int isEmpty2() { return front2 == -1; }
+
+void enqueue1(int x) {
+    if (rear1 == SIZE - 1) { printf("Queue1 Full!\n"); return; }
+    if (front1 == -1) front1 = 0;
+    queue1[++rear1] = x;
+}
+int dequeue1() {
+    if (isEmpty1()) return -1;
+    int val = queue1[front1];
+    if (front1 == rear1) front1 = rear1 = -1;
+    else front1++;
+    return val;
+}
+
+void enqueue2(int x) {
+    if (rear2 == SIZE - 1) { printf("Queue2 Full!\n"); return; }
+    if (front2 == -1) front2 = 0;
+    queue2[++rear2] = x;
+}
+int dequeue2() {
+    if (isEmpty2()) return -1;
+    int val = queue2[front2];
+    if (front2 == rear2) front2 = rear2 = -1;
+    else front2++;
+    return val;
+}
+
+// Push (simple)
+void push(int x) {
+    enqueue1(x);
+    printf("Pushed %d\n", x);
+}
+
+// Pop (costly)
+void pop() {
+    if (isEmpty1()) {
+        printf("Stack Empty!\n");
+        return;
+    }
+
+    // Move all except last element from queue1 to queue2
+    while (front1 != rear1) {
+        enqueue2(dequeue1());
+    }
+
+    // Last element is the "top" of stack
+    printf("Popped %d\n", dequeue1());
+
+    // Swap queue1 and queue2
+    int tempQ[SIZE]; int tempF, tempR;
+    for (int i = 0; i < SIZE; i++) {
+        tempQ[i] = queue1[i];
+        queue1[i] = queue2[i];
+        queue2[i] = tempQ[i];
+    }
+    tempF = front1; front1 = front2; front2 = tempF;
+    tempR = rear1;  rear1 = rear2;  rear2 = tempR;
+}
+
+// Display stack
+void display() {
+    if (isEmpty1()) {
+        printf("Stack Empty!\n");
+        return;
+    }
+    printf("Stack elements: ");
+    for (int i = front1; i <= rear1; i++) {
+        printf("%d ", queue1[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    pop();
+    display();
+
+    push(40);
+    push(50);
+
+    display();
+
+    return 0;
+}
