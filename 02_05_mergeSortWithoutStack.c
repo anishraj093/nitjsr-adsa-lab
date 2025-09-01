f945b215791cf1bc0fb86dd42813ac1b10ddf94d
anishraj093 <131684006+anishraj093@users.noreply.github.com>
2025-09-01 16:53:20 +0530

    02_05

---
+++ 02_05_mergeSortWithoutStack.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// merge two sorted subarrays arr[l..m] and arr[m+1..r]
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[100], R[100];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+// iterative (bottom-up) merge sort
+void mergeSort(int arr[], int n) {
+    int curr_size;   // size of subarrays to be merged
+    int left_start;  // starting index of left subarray
+
+    // Merge subarrays in bottom-up manner
+    for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
+        for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
+            int mid = left_start + curr_size - 1;
+            int right_end = (left_start + 2 * curr_size - 1 < n - 1) ? 
+                             (left_start + 2 * curr_size - 1) : (n - 1);
+
+            if (mid < right_end)  // merge only if two parts exist
+                merge(arr, left_start, mid, right_end);
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    mergeSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
