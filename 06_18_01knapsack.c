bbe5403e13c943a722534809c1205b26df3c7d5e
anishraj093 <131684006+anishraj093@users.noreply.github.com>
2025-09-23 14:44:21 +0530

    commit

---
+++ 06_18_01knapsack.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Function to return maximum of two numbers
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+// 0/1 Knapsack using DP
+int knapSack(int W, int wt[], int val[], int n) {
+    int dp[n+1][W+1];
+
+    // Build table dp[][] in bottom-up manner
+    int i,w;
+    for ( i = 0; i <= n; i++) {
+        for ( w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;  // base case
+            else if (wt[i-1] <= w)
+                dp[i][w] = max(val[i-1] + dp[i-1][w-wt[i-1]], dp[i-1][w]);
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+
+    return dp[n][W];
+}
+
+int main() {
+    int val[] = {60, 100, 120};   // values
+    int wt[]  = {10, 20, 30};     // weights
+    int W = 50;                   // capacity of knapsack
+    int n = sizeof(val) / sizeof(val[0]);
+
+    printf("Maximum value in Knapsack = %d\n", knapSack(W, wt, val, n));
+
+    return 0;
+}
